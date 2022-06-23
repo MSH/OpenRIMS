1191c47437bce1130920de399b99e83c3c7744ec
alexkurasoff <alex.kurasoff@gmail.com>
2022-06-23 21:36:10 +0300

    the first commit

--- pushrepo.cmd
+++ pushrepo.cmd
@@ -1,3 +1,5 @@
+git remote remove origin
+git remote add origin https://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@github.com/MSH/Pharmadex2.git
 git branch -M main
 git add *
 git commit -m "the first commit"
