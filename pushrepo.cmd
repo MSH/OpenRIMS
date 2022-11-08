9ac847d157923875fd49fd39bc5c842da994189c
alexkurasoff <alex.kurasoff@gmail.com>
2022-11-08 14:43:35 +0200

    the first commit

--- pushrepo.cmd
+++ pushrepo.cmd
@@ -1,5 +1,5 @@
 git fetch https://github.com/MSH/Pharmadex2.git main
-git pull https://github.com/MSH/Pharmadex2.git main
+git merge https://github.com/MSH/Pharmadex2.git main
 git branch -M main
 git add *
 git commit -m "the first commit"
